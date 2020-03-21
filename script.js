ec79134d7796ffedd023b3d96768ac32e10dfc2e
PzOng <pangszee.ong@gmail.com>
2020-03-21 23:49:12 +1030

    colorset

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 
 $(document).ready(function(){
     
-    var cday = moment().format('Do MMM YYYY');
+    var cday = moment().format('dddd, Do MMM YYYY, h:mm:ss a');
     $('#currentDay').text(cday);
-    addblock();    
+    addblock();
+    
 
 })
 
 var schedule=[
-    {time: "9 AM", event:""},
-    {time: "10 AM", event:""},
-    {time: "11 AM", event:""},
-    {time: "12 PM", event:""},
-    {time: "1 PM", event:""},
-    {time: "2 PM", event:""},
-    {time: "3 PM", event:""},
-    {time: "4 PM", event:""},
-    {time: "5 PM", event:""}
+    {time: "9 AM", event:"", hour:"09:00"},
+    {time: "10 AM", event:"", hour:"10:00"},
+    {time: "11 AM", event:"", hour:"11:00"},
+    {time: "12 PM", event:"", hour:"12:00"},
+    {time: "1 PM", event:"", hour:"13:00"},
+    {time: "2 PM", event:"", hour:"14:00"},
+    {time: "3 PM", event:"", hour:"15:00"},
+    {time: "4 PM", event:"", hour:"16:00"},
+    {time: "5 PM", event:"", hour:"17:00"}
 ];
 
 function addblock(){
     for(i=0;i<schedule.length;i++){
         var slothour = schedule[i].time;
+        var blockcolor = colorcode(i);
         var entryrow = $('<div class="row"></div>');
         var slotname = $('<div class="col-1 hour"></div>').text(slothour);
-        var texta = $('<textarea class="col-10 form-control description" rows="1"></textarea>"');
+        var texta = $('<textarea class="col-10 form-control '+ blockcolor +' description rows="1">');
         var savebtn = $('<button class="col-1 saveBtn"><i class="far fa-save"></i></button>')
         
         $('#start').append(entryrow);
         entryrow.append(slotname);
         entryrow.append(texta);
         entryrow.append(savebtn);
+        
     }
 }
 
+
+function colorcode(i){
+    var q = moment().format('H A');
+    var w = schedule[i].hour;
+    
+    if(moment(w,'H A').isBefore(q)===true){
+        return "future"
+    }else if(moment(w,'H A').isSame(q)===true){
+        return "present"
+    }else{return "past"}
+
+}
 
