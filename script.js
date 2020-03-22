f2d036bc3f53ad95d0ddc8c9961403e10f6c9f30
PzOng <pangszee.ong@gmail.com>
2020-03-22 15:34:32 +1030

    localstorage logic

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+var usersch = JSON.parse(localStorage.getItem("userdayplan"))
+if(usersch === null){usersch = schedule;}
+
 
 $(document).ready(function(){
+
     
     var cday = moment().format('dddd, Do MMM YYYY, h:mm:ss a');
     $('#currentDay').text(cday);
     addblock();
     
+    $('.saveBtn').on('click',function () {
+        var btnid = $(this).attr('id');
+        console.log(btnid)
+        var p = $(this).prev().val()
+        console.log(p)
+        usersch[btnid].event = p
+        console.log(usersch)
+        localStorage.setItem('userdayplan',JSON.stringify(usersch));
+    })
 
 })
 
@@ -27,8 +40,8 @@ function addblock(){
         var blockcolor = colorMe(hourblock);
         var entryrow = $('<div class="row"'+ "id="+i+">");
         var slotname = $('<div class="col-2 col-lg-1 hour"></div>').text(slothour);
-        var texta = $('<textarea class="col-8 col-lg-10 form-control '+ blockcolor +' description rows="1">');
-        var savebtn = $('<button class="col-2 col-lg-1 saveBtn"><i class="far fa-save"></i></button>')
+        var texta = $('<textarea class="col-8 col-lg-10 form-control description '+ blockcolor +'" rows="1"'+"id="+i+'>').text(usersch[i].event);
+        var savebtn = $('<button class="col-2 col-lg-1 saveBtn"' + 'id='+i+'><i class="far fa-save"></i></button>')
         
         $('#start').append(entryrow);
         entryrow.append(slotname);
@@ -38,22 +51,38 @@ function addblock(){
     }
 }
 
-
 function colorMe(time){
-var x = new Date();
-var y = x.getHours()
-var z = parseInt(time)
-if(y === z){return "present"}
-else if (y > z){return "past"}
-else if (y < z){return "future"}
-}
+    var x = new Date();
+    var y = x.getHours()
+    var z = parseInt(time)
+    if(y === z){return "present"}
+    else if (y > z){return "past"}
+    else if (y < z){return "future"}
+    }
 
 
-function colorcode(time){
-    var hnow = moment()
-    var hslot = moment(time, 'H A')
-    if(hnow.isBefore(hslot)){return "future"}
-    else if(hnow.isSame(hslot)){return "present"}
-    else if(hnow.isAfter(hslot)){return "past"}
+// Redundant
 
-}
+// function colorcode(time){
+//     var hnow = moment()
+//     var hslot = moment(time, 'H A')
+//     if(hnow.isBefore(hslot)){return "future"}
+//     else if(hnow.isSame(hslot)){return "present"}
+//     else if(hnow.isAfter(hslot)){return "past"}
+
+// }
+
+// var hnow = moment()
+// console.log(hnow)
+// var hslot = moment(schedule[0].time, 'H A')
+// console.log(hslot)
+// console.log(hnow.isBefore(hslot))
+// console.log(hnow.isSame(hslot))
+// console.log(hnow.isAfter(hslot))
+
+// var x = new Date()
+// var y = x.getHours()
+// var z = schedule[5].hour
+// var w = parseInt(z)
+
+
